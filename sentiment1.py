49e4bff578d034a20c4a14bd9c4d5f0a64cdec18
subham2903 <60726129+subham2903@users.noreply.github.com>
2021-04-18 10:57:01 +0530

    Delete sentiment1.py

--- sentiment1.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import sklearn
-import joblib
-model = joblib.load('Tweets1')
-st.title('Tweets Classifier')
-ip = st.text_input('Enter your message')
-op = model.predict([ip])
-if st.button('Predict'):
-  st.title(op[0])
